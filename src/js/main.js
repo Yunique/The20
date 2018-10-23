5986d4334d16fb7ce84f9a4d04ff8c4bd4e39387
Yunus <yunique33@gmail.com>
2018-10-24 00:41:44 +0300

    desktop version

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+$('.slider').slick({
+    dots: true,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    adaptiveHeight: true,
+    mobileFirst: true,
+    centerMode: false,
+    slidesToShow: 1,
+    infinite: true,
+    arrows: false,
+});
